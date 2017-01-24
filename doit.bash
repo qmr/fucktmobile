3e6cee291eb7273bac1a5cff64b094fe3569030d
Quintin Riis <quintin@ioffer.com>
2017-01-23 23:36:28 -0800

    useful succint commit message

--- doit.bash
+++ doit.bash
@@ -2,5 +2,7 @@
 bash nmea.bash &
 while true
 do
-	cat 
-
+	location=$(cat location)
+	ping=$(ping -c 1 4.2.2.2 | sed '$!d;s|.*/\([0-9.]*\)/.*|\1|')
+	echo "$location\t$ping" >> log
+done
--- nmea.bash
+++ nmea.bash
@@ -1,4 +1,8 @@
 while true
 do
-	curl 192.168.43.1:10110 | grep GNGGA > location
+	while IFS= read -r line;
+	do
+		location=$(echo "$line" | grep GNGGA)
+		echo "$location" > location
+	done < <(curl 192.168.43.1:10110)
 done
